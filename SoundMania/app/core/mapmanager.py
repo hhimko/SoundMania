a449406ef189ef4cf5c03b54c8d17a18ea19e391
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2022-04-24 21:04:31 +0200

    add simple map parsing

--- SoundMania/app/core/mapmanager.py
+++ SoundMania/app/core/mapmanager.py
@@ -1,19 +1,90 @@
+from dataclasses import dataclass
 import configparser
+import os
+
+import logging
+logger = logging.getLogger("MapManager")
+
+
+@dataclass
+class MapInfo:
+    map_path: str
+    song_author: str
+    song_name: str
+    song_duration: int=0
+    
+    
+    
 
 class MapManager:
-    CONF_PATH = "SoundMania/locals/conf.ini"
+    MAP_EXTENSION = ".smm"
+    INFO_FILE_NAME = "info"
+    
+    CONF_PATH = "SoundMania\\locals\\conf.ini"
+    CONF_DEFAULTS = {
+        "map_dir": "SoundMania\\locals\\maps",
+    }
+    
     
     def __init__(self): 
         self.local_path = self._get_user_path()
+        self._map_cache: dict[str, MapInfo] = {}
+    
+    
+    def load_available_maps(self) -> list[str]:
+        """ Load to the managers cache and return all avaiable map paths. """
+        available = [] 
+        
+        for path in os.listdir(self.local_path):
+            full_path = os.path.join(self.local_path, path)
+            
+            if self._register_map(full_path):
+                available.append(full_path)
+                
+        return available
+    
+    
+    def _register_map(self, path: str) -> bool:
+        map_info = self._parse_map_info(path)
+        if not map_info:
+            return False
+        
+        self._map_cache[path] = map_info
+        return True
     
     
     @classmethod
-    def load_map_index(cls) -> list[None]:
-        return []
+    def _parse_map_info(cls, path: str) -> MapInfo | None:
+        if os.path.isdir(path) and path.endswith(cls.MAP_EXTENSION):
+            info_file_path = os.path.join(path, cls.INFO_FILE_NAME)
+            
+            if not os.path.isfile(info_file_path):
+                logger.warn(f"{path} map exists, but info file is missing")
+                return None
+            
+            with open(info_file_path, 'r') as info_file:
+                author = info_file.readline().strip() or "???"
+                name = info_file.readline().strip() or "???"
+                try:
+                    duration = int(info_file.readline().strip())
+                except ValueError:
+                    logger.warn(f"{path} map exists, but info file is corrupted")
+                    return None 
+            
+            return MapInfo(path, author, name, duration)
+        
+        return None
     
     
     @classmethod
     def _get_user_path(cls) -> str:
         config = configparser.ConfigParser()
         config.read(cls.CONF_PATH)
-        return config["COMMON"]["map_dir"]
+        
+        try:
+            return config["COMMON"]["map_dir"]
+        except KeyError:
+            default = cls.CONF_DEFAULTS["map_dir"]
+            logger.info(f"Could not obtain map directory from 'conf.ini'. Defaulting to '{default}'")
+            return default
+        
